b50de80ff91539dd5a223c85a1b7b15cc24073f9
Nitse <Nitse@hotmail.com>
2022-04-17 19:11:05 +0300

    done functionality, css needed

--- src/components/ExploreContainer.js
+++ src/components/ExploreContainer.js
@@ -1,64 +1,96 @@
-import { IonButton, IonChip, IonDatetime, IonIcon, IonInput, IonItem, IonItemDivider, IonLabel } from '@ionic/react';
+import { IonButton, IonChip, IonIcon, IonInput, IonItem, IonLabel, CreateAnimation, Animation, createAnimation } from '@ionic/react';
 import './ExploreContainer.css';
-import { arrowUndo,logoXbox } from 'ionicons/icons';
+import { arrowUndo, logoXbox, add, closeCircleOutline } from 'ionicons/icons';
 import WheelComponent from 'react-wheel-of-prizes'
+import { swap } from "react-magic";
+import { useState } from 'react'
+import { CSSTransition } from 'react-transition-group';
+import { isPlatform } from '@ionic/react';
 //import 'react-wheel-of-prizes/dist/index.css'
 
 
 function ExploreContainer(props) {
-  const segColors = [
-    '#EE4040',
-    '#F0CF50',
-    '#815CD1',
-    '#3DA5E0',
-    '#34A24F',
-    '#F9AA1F',
-    '#EC3F3F',
-    '#FF9000'
-  ]
+
+  const [anim, setAnim] = useState(false);
+
+  const segColors = []
+  for (let i = 0; i < 10; i++) {
+    segColors.push("#" + Math.floor(Math.random() * 16777215).toString(16));
+  }
+
   const onFinished = (winner) => {
     console.log(winner)
     props.setWinner(winner);
-    props.setShowSpinner(false);
+    setTimeout(() => { props.setShowSpinner(false); }, 2800);
+    setTimeout(() => { setAnim(true); }, 3000);
+    setTimeout(() => { setAnim(false); }, 5000);
+    setTimeout(() => { props.setShowResult(true) }, 5000);
+    //https://www.youtube.com/watch?v=GNSWPnn3Ffs
   }
+  console.log(props.winner)
   return (
     <div className="container">
-      {props.showSpinner ? null : 
-      <>
-      <IonItem>
-        <IonLabel position="floating">Add choice</IonLabel>
-          <IonInput  value={props.choice} onIonChange={e => props.setChoice(e.detail.value)}></IonInput>
-      </IonItem>
-      <IonButton disabled={props.choice === null || props.choice === ''} expand='full' onClick={props.add}>ADD</IonButton>
-      {props.choices.map((c,i)=>{
-        return <IonChip key={i} onClick={props.deleteChoice.bind(this,c)}>
+      {props.showSpinner ? null :
+        <>
+          {props.showResult ?
+            <div id='result2'>
+              <h2>{props.winner}</h2>
+            </div> : null
+          }
+          <IonItem>
+            <IonLabel position="floating">Add choice</IonLabel>
+            <IonInput value={props.choice} onIonChange={e => props.setChoice(e.detail.value)}></IonInput>
+            <IonButton id="addBtn" disabled={props.choice === null || props.choice === ''} expand='block' onClick={props.add} slot={'end'}>
+              <IonIcon icon={add} />
+            </IonButton>
+          </IonItem>
+
+          <div style={{ position: 'absolute' }} className='ion-margin-vertical'>
+            {props.choices.map((c, i) => {
+              let color = Math.floor(Math.random() * 16777215).toString(16);
+              return <IonChip style={{ backgroundColor: "#" + color }} className='chipChoise' key={i} onClick={props.deleteChoice.bind(this, c)}>
                 <IonLabel>{c}</IonLabel>
-                <IonIcon icon={logoXbox} />
+                <IonIcon style={{ fontSize: '30px', color: 'white' }} icon={closeCircleOutline} />
               </IonChip>
-      })}
-      <IonButton disabled={props.choices.length <= 1} expand='full' onClick={props.startSpin}>START</IonButton>
-      <h2>{props.winner}</h2>
-      </>}
-      
+            })}
+          </div>
+          <IonButton id='startButton' disabled={props.choices.length <= 1} expand='full' onClick={props.startSpin}>START</IonButton>
+          {props.winner === null ? null :
+            <CSSTransition
+              in={anim}
+              timeout={300}
+              classNames="winner"
+              unmountOnExit
+              onEnter={() => console.log('on enter')}
+              onExited={() => console.log('on exit')}
+            >
+              <div id='result'>
+                <h2>{props.winner}</h2>
+              </div>
+            </CSSTransition>
+          }
+
+        </>}
+
       {props.showSpinner ?
-        <div>
-        <WheelComponent
-        segments={props.choices}
-        segColors={segColors}
-        //winningSegment='won 10'
-        onFinished={(winner) => onFinished(winner)}
-        primaryColor='black'
-        contrastColor='white'
-        buttonText='Spin'
-        isOnlyOnce={false}
-        size={200}
-        upDuration={100}
-        downDuration={1000}
-        fontFamily='Arial'
-      />
-    <IonButton id='resetButton' shape="round" fill="outline" onClick={props.reset}><IonIcon slot="icon-only" icon={arrowUndo} /></IonButton>
-    </div> : null }
-      
+        <div id={isPlatform('ios') ? 'divWheel' : 'divWheelAndroid'}>
+          <WheelComponent
+            segments={props.choices}
+            segColors={segColors}
+            //winningSegment='won 10'
+            onFinished={(winner) => onFinished(winner)}
+            primaryColor='black'
+            contrastColor='white'
+            buttonText='Spin'
+            isOnlyOnce={false}
+            size={200}
+            upDuration={100}
+            downDuration={1000}
+            fontFamily='Arial'
+          />
+          {/* <IonButton id={isPlatform('ios') ? 'resetButton' : 'resetButtonAndroid'} shape="round" fill="outline" onClick={props.reset}><IonIcon slot="icon-only" icon={arrowUndo} /></IonButton> */}
+        </div> : null}
+
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,29 +9,32 @@ import { useState } from "react";
 import ExploreContainer from "../components/ExploreContainer";
 import "./Home.css";
 
-export function Home  () {
-  const [choices,setChoices] = useState([]);
-  const [choice,setChoice] = useState(null);
-  const [showSpinner,setShowSpinner] = useState(false);
-  const [winner,setWinner] = useState(null);
-  
+export function Home() {
+  const [choices, setChoices] = useState([]);
+  const [choice, setChoice] = useState(null);
+  const [showSpinner, setShowSpinner] = useState(false);
+  const [winner, setWinner] = useState(null);
+  const [showResult, setShowResult] = useState(false);
 
-  const add = () =>{
+
+
+  const add = () => {
     const list = choices;
     list.push(choice);
     setChoices(list);
     setChoice(null);
   }
 
-  const deleteChoice = (c) =>{
-    setChoices( choices.filter(items=>items !== c));
+  const deleteChoice = (c) => {
+    setChoices(choices.filter(items => items !== c));
   }
 
-  const startSpin = () =>{
+  const startSpin = () => {
+    setShowResult(false);
     setShowSpinner(true);
   }
 
-  const reset = () =>{
+  const reset = () => {
     setShowSpinner(false);
     setChoices([]);
   }
@@ -40,18 +43,18 @@ export function Home  () {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Fate</IonTitle>
+          <IonTitle className="ion-text-center">Fate</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
-          <IonToolbar>
+          {/* <IonToolbar>
             <IonTitle size="large">Let me choose</IonTitle>
-          </IonToolbar>
+          </IonToolbar> */}
         </IonHeader>
-        <ExploreContainer setShowSpinner={setShowSpinner} setWinner={setWinner} winner={winner} reset={reset} startSpin={startSpin} showSpinner={showSpinner} deleteChoice={deleteChoice} add={add} choices={choices} setChoices={setChoices} choice={choice} setChoice={setChoice} />
+        <ExploreContainer showResult={showResult} setShowResult={setShowResult} setShowSpinner={setShowSpinner} setWinner={setWinner} winner={winner} reset={reset} startSpin={startSpin} showSpinner={showSpinner} deleteChoice={deleteChoice} add={add} choices={choices} setChoices={setChoices} choice={choice} setChoice={setChoice} />
       </IonContent>
-    </IonPage>
+    </IonPage >
   );
 };
 
